0170ccfdfa27f124e7c76ccb4058770fd5917cae
cassel boris <casselboris92@gmail.com>
2019-12-16 11:36:55 +0100

    creation of an Order class + init function

---
+++ src/com/company/Order.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+public class Order {
+
+    /**
+     * initialization of our programm. No return.
+      */
+    public static void init() {
+
+            System.out.println("Welcome to our library system");
+            System.out.println("Please enter a command");
+    }
+
+    public int processCmd (){
+
+        
+
+    }
+
+
+}
